fa388de62016f531be8e2159a3ec87cf6b8ae632
vuongq303 <vuongq303@gmail.com>
2025-02-01 19:15:45 +0700

    a

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,9 @@
 var express = require("express");
 const authentication = require("../middleware/authentication");
+const executeQuery = require("../sql/promise");
+const config = require("../config/config");
+const jsonwebtoken = require("jsonwebtoken");
+const env = require("../config/env");
 var router = express.Router();
 
 router.get("/", (req, res) => {
@@ -16,15 +20,30 @@ router.get("/phan-quyen", authentication, function (req, res) {
   }
 });
 
-router.get('/auth', authentication, async function (req, res) {
+router.post('/auth', async function (req, res) {
   try {
-    const user = req.user;
-    
+    const { api_key } = req.body;
+    const user = jsonwebtoken.verify(api_key, env.JWT_KEY);
+    const sql = "SELECT id FROM nguoi_dung WHERE tai_khoan = ? AND trang_thai = ?";
+    const result = await executeQuery(sql, [user.tai_khoan, config.dangLamViec]);
+
+    if (result.length > 0) {
+      return res.status(200).json({
+        status: true,
+        message: 'Đăng nhập thành công'
+      })
+    }
+    res.status(200).json({
+      status: false,
+      message: 'Đăng nhập không thành công'
+    })
   } catch (error) {
     console.error('/auth', error.message);
-    res.status(500).json({ status: false });
+    res.status(500).json({
+      status: false,
+      message: 'Đã xảy ra lỗi'
+    });
   }
-
 });
 
 module.exports = router;
